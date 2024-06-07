15e0c3332d75d773ac17a8cba24c1c4ff6fbea19
Hiren Patel <hiren.patel@tatvasoft.com>
2024-06-07 16:11:55 +0530

    change folder of DTO

--- Repository/Services/StackService/Dto/RetrieveStackDataDto.cs
+++ Repository/Services/StackService/Dto/RetrieveStackDataDto.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Repository.Services.UserService
+namespace Repository.Services.StackService.Dto
 {
     public class RetrieveStackDataDto
     {
